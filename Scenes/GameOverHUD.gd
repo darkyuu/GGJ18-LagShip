bc8b5e51156f97df839781ab81c1ce68eb83e056
Kanukwan <haseao112@gmail.com>
2018-01-27 20:22:40 +0700

    Add GameOverHUD.gd, GameOver.tscn

---
+++ Scenes/GameOverHUD.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+signal restart_game
+signal menu_game
+
+
+func show_message(text):
+	$MessageLabel.text = text
+	$MessageLabel.show()
+	$MessageLabel.start()
+
+func show_gameover():
+	show_message("GAME OVER")
+
+func _on_RestartButton_pressed():
+	print("_on_RestartButton_pressed")
+	emit_signal("restart_game")
+
+func _on_MenuButton_pressed():
+	print("_on_MenuButton_pressed")
+	emit_signal("menu_game")
